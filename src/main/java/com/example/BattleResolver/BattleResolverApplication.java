f7e8b6e32dfef4af04adf174ea76b447cd5a831d
d8d8d8d8sk <98910432+d8d8d8d8sk@users.noreply.github.com>
2022-03-10 23:03:19 -0800

    add base proj

---
+++ src/main/java/com/example/BattleResolver/BattleResolverApplication.java
@@ -0,0 +1,13 @@
+package com.example.BattleResolver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BattleResolverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BattleResolverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BattleResolver/BattleResolverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BattleResolver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BattleResolverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
